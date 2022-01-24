5537957baa78a15c50b5304ee5a70f1726683943
Flavia Nitto <flavia.nitto@accurate.com.br>
2022-01-24 13:05:17 -0300

    versão final

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let legenda = document.querySelector('p#conteudo__ocorrencias');
 let sel = document.getElementById('selregiaoC');
 let selC = document.getElementById('selcriminalidade');
 
+let selM = document.getElementById('selregiaoM');
+let crimM = document.getElementById('selcrimM')
+
 let regioes = [];
 regioes.push('Aricanduva');
 regioes.push('Butantã');
@@ -52,6 +55,45 @@ function buscarC() {
         break;
       case 3: //acidente
         window.open('./acidenteB.html', "_self");
+        break;
+    }    
+  }
+}
+
+//buscar modelos
+function buscarModelo() {
+  let regm = selM.options.selectedIndex;
+  let crim = crimM.options.selectedIndex;
+  alert('Buscando modelo da subprefeitura de ' + regioes[regm] + ' \nCrime selecionado: ' + crimes[crim]);
+  if (regm == 0) {
+    switch (crim) {
+      case 0: //roubo
+        window.open('./rouboMA.html', "_self");
+        break;
+      case 1: //estupro
+        window.open('./estuproMA.html', "_self");
+        break;
+      case 2: //homicidio
+        window.open('./homicidioMA.html', "_self");
+        break;
+      case 3: //acidente
+        window.open('./acidenteMA.html', "_self");
+        break;
+    }
+  }
+  else {
+    switch (crim) {
+      case 0: //roubo
+        window.open('./rouboMB.html', "_self");
+        break;
+      case 1: //estupro
+        window.open('./estuproMB.html', "_self");
+        break;
+      case 2: //homicidio
+        window.open('./homicidioMB.html', "_self");
+        break;
+      case 3: //acidente
+        window.open('./acidenteMB.html', "_self");
         break;
     }    
   }
